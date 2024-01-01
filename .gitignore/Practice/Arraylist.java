342e1da9587e658ffc6aa0fe6e66b42ba08e9350
Harsh1101 <sinha.harshraj1101@gmail.com>
2024-01-02 00:00:51 +0530

    adding git ignore

---
+++ .gitignore/Practice/Arraylist.java
@@ -0,0 +1,30 @@
+// Q.1 Create an ArrayList of integers and add the numbers 1 to 5 to the list. Print the elements.
+/*Q.2 Create an ArrayList without specifying the initial capacity. Add 10 elements to the list. Print the size and capacity of the list. Add 5 more elements and print the updated size and capacity. Observe how the capacity changes.
+*/
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+public class Arraylist {
+    public static void main(String[] args) {
+        ArrayList al = new ArrayList();
+        al.add(1);
+        al.add(2);
+        al.add(3);
+        al.add(4);
+        al.add(5);
+        System.out.println(al);
+        int capacity = getArrayListCapacity(al);
+        System.out.println("Initial Capacity: "+capacity);
+    }
+
+    private static int getArrayListCapacity(ArrayList al) {
+        try{
+            Field  field = ArrayList.class.getDeclaredField("elementData");
+            field.setAccessible(true);
+            Object[] elementData = (Object[]) field.get(al);
+            return elementData.length;
+        }catch(NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+}
---
+++ .gitignore/Practice/basic.java
@@ -0,0 +1,8 @@
+public class basic{
+    public static void main(String[] args) {
+        char ch = 'a';
+        char ch1 = 'b';
+        System.out.println(ch + ch1);
+        System.out.println('a'+'b');
+    }
+}
