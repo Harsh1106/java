bee7438f52890dcb190db454e4ad3d6f7f6ca485
harsh <sinha.harshraj1101@gmail.com>
2024-07-16 01:35:41 +0530

    Implemented the selection procedure and learned how to find kth largest element from array

---
+++ Assingments/Sarching & Sorting/Number System/q1.java
@@ -0,0 +1,12 @@
+public class q1{
+    public static boolean sum(int[] arr, int k){
+        for(int i = 0 ; i < arr.length ; i++){
+            
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,3,2,2};
+        int k = 2;
+        boolean poss = sum(arr, k);
+    }
+}
