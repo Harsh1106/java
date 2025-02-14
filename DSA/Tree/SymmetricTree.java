c619160fce3749062708425d6d9a8847f1cf760c
harsh <sinha.harshraj1101@gmail.com>
2025-02-14 19:39:00 +0530

    101. Symmetric Tree

---
+++ DSA/Tree/SymmetricTree.java
@@ -0,0 +1,33 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+public class SymmetricTree {
+    public static boolean check(TreeNode left, TreeNode right){
+        if(left == null && right == null) return true;
+        if(left == null || right == null) return false;
+        if(left.val != right.val) return false;
+        return check(left.left, right.right) && check(left.right, right.left);
+    }
+    public static boolean isSymetric(TreeNode root){
+        return check(root, root);
+    }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);   
+        System.out.println(isSymetric(root));
+    }
+}
