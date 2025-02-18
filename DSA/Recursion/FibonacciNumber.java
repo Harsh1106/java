e6c0946986e024e79622c88a61356ec878711369
harsh <sinha.harshraj1101@gmail.com>
2025-02-18 21:59:02 +0530

    509. Fibonacci Number

---
+++ DSA/Recursion/FibonacciNumber.java
@@ -0,0 +1,9 @@
+public class FibonacciNumber{
+    public static int fib(int n){
+        if(n <= 1) return n;
+        return fib(n - 1) + fib(n - 2);
+    }
+    public static void main(String[] args) {
+        System.out.println(fib(4));
+    }
+}
