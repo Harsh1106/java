c94994162cd98eb26ce16fa61d1c68761e4a5b81
harsh <sinha.harshraj1101@gmail.com>
2025-02-21 22:09:05 +0530

    680. Valid Palindrome II

---
+++ StringPractice/ValidPallindrome2.java
@@ -0,0 +1,30 @@
+public class ValidPallindrome2 {
+    private  static boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public static boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return isPalindrome(s, left + 1, right) || 
+                       isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String s = "aba";
+        System.out.println(validPalindrome(s));
+    }
+}
